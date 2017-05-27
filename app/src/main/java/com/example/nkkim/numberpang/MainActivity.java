e8dbf881cedc180bde95188dccccf560b1e2b42a
rlaskrab09 <rlaskarb09@naver.com>
2017-05-28 02:38:02 +0900

    Button

--- app/src/main/java/com/example/nkkim/numberpang/MainActivity.java
+++ app/src/main/java/com/example/nkkim/numberpang/MainActivity.java
@@ -9,20 +9,12 @@ package com.example.nkkim.numberpang;
         import android.view.View;
         import android.widget.TextView;
         import android.widget.Toast;
+        import java.util.Random;
 
         import static android.R.id.text1;
 
 public class MainActivity extends AppCompatActivity {
-
-    GestureDetector mGestures = null;
-
-    public boolean onTouchEvent(MotionEvent event) {
-        if(mGestures != null) {
-            return mGestures.onTouchEvent(event);
-        } else {
-            return super.onTouchEvent(event);
-        }
-    }
+    int ranNumber = 1;
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return super.onKeyDown(keyCode, event);
@@ -33,14 +25,90 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView text1 = (TextView) findViewById(R.id.text1);
+        final TextView text1 = (TextView) findViewById(R.id.text1);
         text1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "hihi", Toast.LENGTH_LONG).show();
+                text1.setText(ranNumber + "");
+                newRandom();
             }
         });
 
+        final TextView text2 = (TextView) findViewById(R.id.text2);
+        text2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text2.setText(ranNumber + "");
+                newRandom();
+            }
+        });
 
+        final TextView text3 = (TextView) findViewById(R.id.text3);
+        text3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text3.setText(ranNumber + "");
+                newRandom();
+            }
+        });
+
+        final TextView text4 = (TextView) findViewById(R.id.text4);
+        text4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text4.setText(ranNumber + "");
+                newRandom();
+            }
+        });
+
+        final TextView text5 = (TextView) findViewById(R.id.text5);
+        text5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text5.setText(ranNumber + "");
+                newRandom();
+            }
+        });
+
+        final TextView text6 = (TextView) findViewById(R.id.text6);
+        text6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text6.setText(ranNumber + "");
+                newRandom();
+            }
+        });
+
+        final TextView text7 = (TextView) findViewById(R.id.text7);
+        text7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text7.setText(ranNumber + "");
+                newRandom();
+            }
+        });
+
+        final TextView text8 = (TextView) findViewById(R.id.text8);
+        text8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text8.setText(ranNumber + "");
+                newRandom();
+            }
+        });
+
+        final TextView text9 = (TextView) findViewById(R.id.text9);
+        text9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text9.setText(ranNumber + "");
+                newRandom();
+            }
+        });
+    }
+
+    void newRandom() {
+        Random r = new Random();
+        this.ranNumber = r.nextInt(5) + 1;
     }
 }
