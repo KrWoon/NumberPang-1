8371f75f9bf87651677abf797e9ee6e807ec6e6e
rlaskrab09 <rlaskarb09@naver.com>
2017-05-28 02:17:06 +0900

    Button

--- app/src/main/java/com/example/nkkim/numberpang/MainActivity.java
+++ app/src/main/java/com/example/nkkim/numberpang/MainActivity.java
@@ -3,15 +3,44 @@ package com.example.nkkim.numberpang;
         import android.app.ActionBar;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.GestureDetector;
+        import android.view.KeyEvent;
+        import android.view.MotionEvent;
+        import android.view.View;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import static android.R.id.text1;
 
 public class MainActivity extends AppCompatActivity {
+
+    GestureDetector mGestures = null;
+
+    public boolean onTouchEvent(MotionEvent event) {
+        if(mGestures != null) {
+            return mGestures.onTouchEvent(event);
+        } else {
+            return super.onTouchEvent(event);
+        }
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
+
+        TextView text1 = (TextView) findViewById(R.id.text1);
+        text1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "hihi", Toast.LENGTH_LONG).show();
+            }
+        });
+
 
     }
 }
